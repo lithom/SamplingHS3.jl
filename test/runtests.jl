db29b32e6f4f3f92d3b148ae33ddca70cc715a40
lithomas <liphardtt@gmail.com>
2018-07-19 13:11:31 +0200

    Added adaptive mcmc sampling test

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,8 @@ end
 @testset "HR Tests" begin include("test_hr.jl") end
 @testset "MH Tests" begin include("test_mh.jl") end
 @testset "NS Integration" begin include("test_ns_integration_01.jl") end
+@testset "Adaptive MCMC" begin include("test_adaptive_mcmc.jl") end
 @testset "SamplingHS3 Utils 01" begin include("test_utils_01.jl") end
+
 
 #@testset "HR Tests" begin include("test_hr.jl") end
